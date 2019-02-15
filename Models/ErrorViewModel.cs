511a30b26204febaeb19a6bd855b047e7abf465b
Brunno Pleffken <brunno.pleffken@outlook.com>
2019-02-15 12:39:06 -0200

    Initial project structure

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Trackr.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
